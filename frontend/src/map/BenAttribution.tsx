64c0c87e1523449c85c4b627ccc72f63d1247a18
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-03-10 17:05:23 +0000

    feat(frontend): update author attribution

--- frontend/src/map/BenAttribution.tsx
+++ frontend/src/map/BenAttribution.tsx
@@ -2,14 +2,14 @@ export const BenAttribution = () => {
   return (
     <div className=" flex gap-2 absolute top-0 right-0 bg-slate-500 text-slate-900 bg-opacity-20 rounded-full px-3 py-1 m-2 text-xs">
       <p>
-        powered by{" "}
+        a 100% free app, by{" "}
         <a
           className="hover:underline hover:text-slate-700"
           target="_blank"
           rel="noreferrer"
           href="https://orth.uk/"
         >
-          Ben Butterworth
+          Ben Butterworth 💙️
         </a>
       </p>
     </div>
