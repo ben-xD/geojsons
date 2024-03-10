54fb4e1a2587285fe130e54295fb35cd938e337f
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-03-10 19:11:35 +0000

    feat(frontend): move author attribution to left on narrow screens

--- frontend/src/map/BenAttribution.tsx
+++ frontend/src/map/BenAttribution.tsx
@@ -1,6 +1,6 @@
 export const BenAttribution = () => {
   return (
-    <div className=" flex gap-2 absolute top-0 right-0 bg-slate-500 text-slate-900 bg-opacity-20 rounded-full px-3 py-1 m-2 text-xs">
+    <div className=" flex gap-2 absolute top-0 left-0 md:left-auto md:right-0 bg-slate-500 text-slate-900 bg-opacity-20 rounded-full px-3 py-1 m-2 text-xs">
       <p>
         a 100% free app, by{" "}
         <a
