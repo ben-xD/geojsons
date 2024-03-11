be1aa65710b8c06ace995542cb3e3a224a6459fb
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-03-11 21:30:13 +0000

    feat(frontend): add drag handle for raw geojson panel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { GeojsonsMap } from "./map/GeojsonsMap.tsx";
 import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import { RawGeojsonPanel } from "@/GeojsonPanel/RawGeojsonPanel.tsx";
+import { GripHorizontal } from "lucide-react";
 
 function App() {
   return (
@@ -10,7 +11,15 @@ function App() {
           <Panel>
             <GeojsonsMap />
           </Panel>
-          <PanelResizeHandle />
+          <PanelResizeHandle className="w-full h-px bg-slate-300 flex items-center justify-center">
+            <div className="h-4 drop-shadow w-5 z-10 bg-slate-200 rounded-md px-0.5 flex items-center justify-center">
+              <GripHorizontal
+                className="text-slate-600"
+                height={24}
+                width={24}
+              />
+            </div>
+          </PanelResizeHandle>
           <Panel style={{ overflow: "auto" }} defaultSize={40}>
             <RawGeojsonPanel />
           </Panel>
