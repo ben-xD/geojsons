9277c6cdf3ef1e524959c67c58436abf187adadc
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2024-03-26 10:30:33 +0000

    chore(frontend): add dependencies for prettier-eslint extension

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -8,6 +8,7 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
